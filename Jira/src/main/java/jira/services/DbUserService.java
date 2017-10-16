ab318c200b77826ef41acb1c0c1d73d1eb2369e2
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-16 23:25:46 +0300

    add User Service

---
+++ Jira/src/main/java/jira/services/DbUserService.java
@@ -0,0 +1,66 @@
+package jira.services;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Service;
+
+import jira.contract.IUserService;
+import jira.db.HibernateUtils;
+import jira.exceptions.EmailException;
+import jira.exceptions.UserException;
+import jira.models.User;
+
+@Service
+public class DbUserService implements IUserService {
+	static int userId;
+
+	@Override
+	public User signIn(String email, String password) throws UserException, NoSuchAlgorithmException {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Criteria criteria = session.createCriteria(User.class);
+		criteria.add(Restrictions.eq("email", email));
+		User user = (User) criteria.uniqueResult();
+		if (user != null && user.getPassword().equals(getEncryptedPassword(password))) {
+			DbUserService.userId = 1;
+			session.close();
+			return user;
+		}
+		session.close();
+		throw new UserException("Invalid email or password");
+	}
+
+	@Override
+	public User singUp(String email, String password) throws EmailException, NoSuchAlgorithmException {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		Transaction transaction = session.beginTransaction();
+		Criteria criteria = session.createCriteria(User.class);
+		criteria.add(Restrictions.eq("email", email));
+		Object object = criteria.uniqueResult();
+		if (object != null) {
+			throw new EmailException("Email already exists");
+		}
+		User newUser = new User();
+		newUser.setEmail(email);
+		newUser.setPassword(getEncryptedPassword(password));
+		session.save(newUser);
+		transaction.commit();
+		session.close();
+		return newUser;
+	}
+
+	@SuppressWarnings("restriction")
+	private final static String getEncryptedPassword(final String clearTextPassword) throws NoSuchAlgorithmException {
+
+		
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(clearTextPassword.getBytes());
+			return new sun.misc.BASE64Encoder().encode(md.digest());
+		
+	}
+
+}
