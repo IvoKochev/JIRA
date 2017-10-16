2a0e12306f60b85b5ff80b9be99a6e2ac8d9b37f
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-16 21:35:28 +0300

    add HibernateUtils

---
+++ Jira/src/main/java/jira/db/HibernateUtils.java
@@ -0,0 +1,36 @@
+package jira.db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import jira.models.Issue;
+import jira.models.Project;
+import jira.models.Sprint;
+import jira.models.User;
+
+public class HibernateUtils {
+	private static SessionFactory sessionFactory;
+
+	static {
+		try {
+			Configuration configuration = new Configuration().configure();
+			configuration.addAnnotatedClass(Issue.class);
+			configuration.addAnnotatedClass(Project.class);
+			configuration.addAnnotatedClass(Sprint.class);
+			configuration.addAnnotatedClass(User.class);
+			StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
+			serviceRegistryBuilder.applySettings(configuration.getProperties());
+			StandardServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
