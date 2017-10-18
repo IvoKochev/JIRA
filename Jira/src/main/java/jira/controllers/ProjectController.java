f94075f2220f3efb88acacb2c0bcfc48b7107068
Ivo Kochev <ivo>
2017-10-18 14:55:33 +0300

    Fixing project controller

--- Jira/src/main/java/jira/controllers/ProjectController.java
+++ Jira/src/main/java/jira/controllers/ProjectController.java
@@ -2,12 +2,15 @@ package jira.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import jira.contracts.IProjectService;
+import jira.exceptions.ResourceNotFoundException;
 import jira.models.Project;
 
 @RestController
@@ -20,9 +23,9 @@ public class ProjectController {
 		this.projectService = projectService;
 	}
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public List<Project> getProjectList() {
-		return this.projectService.getProjectList();
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public List<Project> getProjectList(HttpServletRequest request) throws ResourceNotFoundException {
+		return this.projectService.getProjectList(request);
 	}
 
 }
