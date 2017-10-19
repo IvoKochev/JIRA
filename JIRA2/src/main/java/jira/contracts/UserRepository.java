5843d5cc6f403c1c7db7a2117563b691a79a8c57
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-19 16:30:21 +0300

    big refactoring , transfer files in new maven web project

--- JIRA2/src/main/java/jira/contracts/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package jira.contracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jira.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
