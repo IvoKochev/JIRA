a4c4eddd078421789bd1a67effa9d3afffb1c3e9
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-21 17:06:52 +0300

    add pageing

--- JIRA3/src/main/resources/public/js/controllers.js
+++ JIRA3/src/main/resources/public/js/controllers.js
@@ -1,9 +1,36 @@
 angular.module('jira.controllers', [])
   .controller('jiraCtrl', function($scope, projectService) {
-    console.log("In Jira Controoler");
+    $scope.page = 1;
+    var pageSize = 20;
+    var projects;
+    $scope.isPrevDisabled = true;
+    $scope.isNextDisabled = false;
+
+    $scope.goToPrevPage = function() {
+      if ($scope.page === 1) {
+        return;
+      }
+      --$scope.page;
+      if ($scope.page === 1) {
+        $scope.isPrevDisabled = true;
+      }
+      $scope.isNextDisabled = false;
+      $scope.projects = projects.slice(($scope.page - 1) * pageSize, $scope.page * pageSize);
+    }
+    $scope.goToNextPage = function() {
+      if ($scope.page === projects.length / pageSize) {
+        return;
+      }
+      ++$scope.page;
+      if ($scope.page === projects.length / pageSize) {
+        $scope.isNextDisabled = true;
+      }
+      $scope.isPrevDisabled = false;
+      $scope.projects = projects.slice(($scope.page - 1) * pageSize, $scope.page * pageSize);
+    }
     projectService.all(function(data) {
-      console.log(data);
-      $scope.projects = data;
+      projects = data;
+      $scope.projects = projects.slice(($scope.page - 1) * pageSize, $scope.page * pageSize);
 
     });
   });
