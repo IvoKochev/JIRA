e0e1932ef72c619e650dbbac02a90d77199734c2
Ivo <ivodkochev@gmail.com>
2017-10-23 12:59:35 +0300

    Fixing Sprint model

--- JIRA3/src/main/java/com/jira/model/Sprint.java
+++ JIRA3/src/main/java/com/jira/model/Sprint.java
@@ -24,6 +24,8 @@ public class Sprint implements Serializable {
 	private int id;
 	@Column(name = "name")
 	private String name;
+	@Column(name = "owner_id")
+	private Integer uploader_id;
 	@Column(name = "sprintgoal")
 	private String sprintgoal;
 	@Column(name = "start_date")
@@ -69,5 +71,57 @@ public class Sprint implements Serializable {
 
 	public void setEnd_date(LocalDateTime end_date) {
 		this.end_date = end_date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((end_date == null) ? 0 : end_date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sprintgoal == null) ? 0 : sprintgoal.hashCode());
+		result = prime * result + ((start_date == null) ? 0 : start_date.hashCode());
+		result = prime * result + ((uploader_id == null) ? 0 : uploader_id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sprint other = (Sprint) obj;
+		if (end_date == null) {
+			if (other.end_date != null)
+				return false;
+		} else if (!end_date.equals(other.end_date))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sprintgoal == null) {
+			if (other.sprintgoal != null)
+				return false;
+		} else if (!sprintgoal.equals(other.sprintgoal))
+			return false;
+		if (start_date == null) {
+			if (other.start_date != null)
+				return false;
+		} else if (!start_date.equals(other.start_date))
+			return false;
+		if (uploader_id == null) {
+			if (other.uploader_id != null)
+				return false;
+		} else if (!uploader_id.equals(other.uploader_id))
+			return false;
+		return true;
 	}
 }
