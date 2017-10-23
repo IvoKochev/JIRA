03ccd43a34d62b4879e4552ae67519d5807e1452
Ivo <ivodkochev@gmail.com>
2017-10-23 11:46:26 +0300

    Adding attachment service implementation

---
+++ JIRA3/src/main/java/com/jira/service/AttachmentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.jira.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+import com.jira.exceptions.ResourceNotFoundException;
+import com.jira.model.Attachment;
+
+public class AttachmentServiceImpl{
+
+
+
+}
