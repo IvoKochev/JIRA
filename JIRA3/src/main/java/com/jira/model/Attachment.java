3cd247d5144d5ec2dab3332f14cdb106acc24753
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-24 18:48:42 +0300

    fix2

--- JIRA3/src/main/java/com/jira/model/Attachment.java
+++ JIRA3/src/main/java/com/jira/model/Attachment.java
@@ -1,68 +1,4 @@
-<<<<<<< HEAD
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.jira.model;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-///**
-// *
-// * @author ivo
-// */
-//@Entity
-//@Table(name = "attachments")
-//public class Attachment {
-//	
-//	private int id;
-//	private Integer uploader_id;
-//	private String location;
-//	private Integer issues_id;
-//	
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	@Column(name = "id")
-//	public int getId() {
-//		return id;
-//	}
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	
-//	@Column(name = "location")
-//	public String getLocation() {
-//		return location;
-//	}
-//	public void setLocation(String location) {
-//		this.location = location;
-//	}
-//	
-//	@Column(name = "issues_id")
-//	public Integer getIssuesid() {
-//		return issues_id;
-//	}
-//	public void setIssues_id(Integer issues_id) {
-//		this.issues_id = issues_id;
-//	}
-//	
-//	@Column(name = "uploader_id")
-//	public Integer getUploaderid() {
-//		return uploader_id;
-//	}
-//	public void setUploaderid(Integer uploaderid) {
-//		this.uploader_id = uploaderid;
-//	}
-//	
-//    
-//}
-=======
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -127,4 +63,4 @@ public class Attachment {
 	
     
 }
->>>>>>> refs/remotes/origin/master
+
