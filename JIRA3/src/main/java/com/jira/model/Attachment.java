2db89e585d9c48e6e896ccd1021c5ccace488eeb
Ivo <ivodkochev@gmail.com>
2017-10-27 00:33:30 +0300

    Adding all joins on issue sprints and attachments

--- JIRA3/src/main/java/com/jira/model/Attachment.java
+++ JIRA3/src/main/java/com/jira/model/Attachment.java
@@ -11,6 +11,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -24,7 +26,7 @@ public class Attachment {
 	private Integer id;
 	private Integer uploader_id;
 	private String location;
-	private Integer issues_id;
+	private Issue issue;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -44,14 +46,6 @@ public class Attachment {
 		this.location = location;
 	}
 	
-	@Column(name = "issues_id")
-	public Integer getIssues_id() {
-		return issues_id;
-	}
-	
-	public void setIssues_id(Integer issues_id) {
-		this.issues_id = issues_id;
-	}
 	
 	@Column(name = "uploader_id")
 	public Integer getUploaderid() {
@@ -59,6 +53,58 @@ public class Attachment {
 	}
 	public void setUploaderid(Integer uploaderid) {
 		this.uploader_id = uploaderid;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "issues_id")
+	public Issue getIssue() {
+		return issue;
+	}
+	public void setIssue(Issue issue) {
+		this.issue = issue;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((issue == null) ? 0 : issue.hashCode());
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((uploader_id == null) ? 0 : uploader_id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Attachment other = (Attachment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (issue == null) {
+			if (other.issue != null)
+				return false;
+		} else if (!issue.equals(other.issue))
+			return false;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		if (uploader_id == null) {
+			if (other.uploader_id != null)
+				return false;
+		} else if (!uploader_id.equals(other.uploader_id))
+			return false;
+		return true;
 	}
 	
     
--- JIRA3/src/main/java/com/jira/model/Sprint.java
+++ JIRA3/src/main/java/com/jira/model/Sprint.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,10 +23,11 @@ public class Sprint implements Serializable {
 	
 	private int id;
 	private String name;
-	private Integer ownerid;
+	private Integer owner_id;
 	private String sprintgoal;
 	private LocalDateTime start_date;
 	private LocalDateTime end_date;
+	private Project project;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -72,14 +75,84 @@ public class Sprint implements Serializable {
 	public void setEnd_date(LocalDateTime end_date) {
 		this.end_date = end_date;
 	}
-	
+
 	@Column(name = "owner_id")
-	public Integer getOwnerid() {
-		return ownerid;
+	public Integer getOwner_id() {
+		return owner_id;
 	}
 
-	public void setOwnerid(Integer ownerid) {
-		this.ownerid = ownerid;
+	public void setOwner_id(Integer owner_id) {
+		this.owner_id = owner_id;
 	}
+
+	@ManyToOne
+	@JoinColumn(name = "projects_id")
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((end_date == null) ? 0 : end_date.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((owner_id == null) ? 0 : owner_id.hashCode());
+		result = prime * result + ((project == null) ? 0 : project.hashCode());
+		result = prime * result + ((sprintgoal == null) ? 0 : sprintgoal.hashCode());
+		result = prime * result + ((start_date == null) ? 0 : start_date.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sprint other = (Sprint) obj;
+		if (end_date == null) {
+			if (other.end_date != null)
+				return false;
+		} else if (!end_date.equals(other.end_date))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (owner_id == null) {
+			if (other.owner_id != null)
+				return false;
+		} else if (!owner_id.equals(other.owner_id))
+			return false;
+		if (project == null) {
+			if (other.project != null)
+				return false;
+		} else if (!project.equals(other.project))
+			return false;
+		if (sprintgoal == null) {
+			if (other.sprintgoal != null)
+				return false;
+		} else if (!sprintgoal.equals(other.sprintgoal))
+			return false;
+		if (start_date == null) {
+			if (other.start_date != null)
+				return false;
+		} else if (!start_date.equals(other.start_date))
+			return false;
+		return true;
+	}
+	
+	
 
 }
