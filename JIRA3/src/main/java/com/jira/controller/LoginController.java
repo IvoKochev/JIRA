63d501d73df84dd2cd6c00445afa2d73961b2471
S_Vatahov <slavchovatahov94@gmail.com>
2017-10-28 17:55:04 +0300

    login changes

--- JIRA3/src/main/java/com/jira/controller/LoginController.java
+++ JIRA3/src/main/java/com/jira/controller/LoginController.java
@@ -48,6 +48,7 @@ public class LoginController {
 		if (bindingResult.hasErrors()) {
 			modelAndView.setViewName("registration");
 		} else {
+			user.setImgurl("/images/user.png");
 			userService.saveUser(user);
 			modelAndView.addObject("successMessage", "User has been registered successfully");
 			modelAndView.addObject("user", new User());
