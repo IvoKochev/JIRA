67574e16b607c8186e2f5ac42ac12ac696ddc289
S_Vatahov <slavchovatahov94@gmail.com>
2017-11-01 04:51:59 +0200

    sprint controller refactoring

--- JIRA3/src/main/java/com/jira/controller/IssueController.java
+++ JIRA3/src/main/java/com/jira/controller/IssueController.java
@@ -22,17 +22,16 @@ import com.jira.repository.UserRepository;
 
 @RestController
 public class IssueController {
-        @Autowired
-        private IProjectService projectService;
+	@Autowired
+	private IProjectService projectService;
 	@Autowired
 	private IIssueService issueService;
 	@Autowired
 	private ISprintService sprintService;
 	@Autowired
 	IssueRepository issueRepository;
-        @Autowired
-        UserRepository userRepository;
-        
+	@Autowired
+	UserRepository userRepository;
 
 	@RequestMapping(value = "/common/createIssue", method = RequestMethod.GET)
 	public ModelAndView issueCreateTemplate() {
@@ -68,49 +67,49 @@ public class IssueController {
 	@RequestMapping(value = "/common/issueView/{id}", method = RequestMethod.GET)
 	public Issue getIssue(@PathVariable(value = "id") int id) {
 		Issue issue = issueRepository.findById(id);
-                issue.setAsignee(userRepository.findByid(issue.getAsignee_id()));
-                issue.setReporter(userRepository.findByid(issue.getReporter_id()));
+		issue.setAsignee(userRepository.findByid(issue.getAsignee_id()));
+		issue.setReporter(userRepository.findByid(issue.getReporter_id()));
 		return issue;
 	}
-        
-        @RequestMapping(value = "/setAsignee", method = RequestMethod.POST)
-        public ModelAndView setAsignee(HttpServletRequest request) {
-            String email = request.getParameter("asignee");
-            int id = Integer.parseInt(request.getParameter("issueId"));
-            Issue issue = issueService.getIssue(id);
-            Sprint sprint = sprintService.findSprintById(issue.getSprint().getId());
-            Project project = projectService.getProjectById(sprint.getProject().getId());
-            User user = userRepository.findByEmail(email);
-            if(user != null && user.getProjects().contains(project)) {
-                issue.setAsignee_id(user.getId());
-                issueService.saveIssue(issue);
-            }
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("redirect:/common/home#!/sprintView/" + id);
-            return modelAndView;
-        }
-       
-        @RequestMapping(value = "/setStatus", method = RequestMethod.POST)
-        public ModelAndView setStatus(HttpServletRequest request) {
-            int id = Integer.parseInt(request.getParameter("issueId"));
-            String status = request.getParameter("status");
-            Issue issue = issueService.getIssue(id);
-            issue.setStatus(status);
-            issueService.saveIssue(issue);
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("redirect:/common/home#!/issueView/" + id);
-            return modelAndView;
-        }
-        
-        @RequestMapping(value = "/setPriority", method = RequestMethod.POST)
-        public ModelAndView setPriority(HttpServletRequest request) {
-            String priority = request.getParameter("priority");
-            int id = Integer.parseInt(request.getParameter("issueId"));
-            Issue issue = issueService.getIssue(id);
-            issue.setPriority(priority);
-            issueService.saveIssue(issue);
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.setViewName("redirect:/common/home#!/issueView/" + id);
-            return modelAndView;
-        }
+
+	@RequestMapping(value = "/setAsignee", method = RequestMethod.POST)
+	public ModelAndView setAsignee(HttpServletRequest request) {
+		String email = request.getParameter("asignee");
+		int id = Integer.parseInt(request.getParameter("issueId"));
+		Issue issue = issueService.getIssue(id);
+		Sprint sprint = sprintService.findSprintById(issue.getSprint().getId());
+		Project project = projectService.getProjectById(sprint.getProject().getId());
+		User user = userRepository.findByEmail(email);
+		if (user != null && user.getProjects().contains(project)) {
+			issue.setAsignee_id(user.getId());
+			issueService.saveIssue(issue);
+		}
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("redirect:/common/home#!/sprintView/" + id);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/setStatus", method = RequestMethod.POST)
+	public ModelAndView setStatus(HttpServletRequest request) {
+		int id = Integer.parseInt(request.getParameter("issueId"));
+		String status = request.getParameter("status");
+		Issue issue = issueService.getIssue(id);
+		issue.setStatus(status);
+		issueService.saveIssue(issue);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("redirect:/common/home#!/issueView/" + id);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/setPriority", method = RequestMethod.POST)
+	public ModelAndView setPriority(HttpServletRequest request) {
+		String priority = request.getParameter("priority");
+		int id = Integer.parseInt(request.getParameter("issueId"));
+		Issue issue = issueService.getIssue(id);
+		issue.setPriority(priority);
+		issueService.saveIssue(issue);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("redirect:/common/home#!/issueView/" + id);
+		return modelAndView;
+	}
 }
--- JIRA3/src/main/java/com/jira/controller/SprintController.java
+++ JIRA3/src/main/java/com/jira/controller/SprintController.java
@@ -10,12 +10,13 @@ import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jira.contract.IProjectService;
 import com.jira.contract.ISprintService;
@@ -29,7 +30,7 @@ import com.jira.model.Sprint;
  *
  * @author ivo
  */
-@RestController
+@Controller
 public class SprintController {
 
 	@Autowired
@@ -37,37 +38,33 @@ public class SprintController {
 	@Autowired
 	private IProjectService projectService;
 
-	@RequestMapping(value = "/common/createSprint", method = RequestMethod.GET)
-	public ModelAndView getSprint() throws ResourceNotFoundException {
+	@GetMapping("/common/createSprint")
+	public String getSprint(Model model) throws ResourceNotFoundException {
 		Sprint sprint = new Sprint();
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("sprint", sprint);
-		modelAndView.setViewName("common/createSprint");
-		return modelAndView;
+		model.addAttribute("sprint", sprint);
+		return "common/createSprint";
 	}
 
-	@RequestMapping(value = "/createSprint", method = RequestMethod.POST)
-	public ModelAndView createSprint(@ModelAttribute Sprint sprint, HttpServletRequest request) throws SprintException {
+	@PostMapping("/createSprint")
+	public String createSprint(Model model, @ModelAttribute("sprint") Sprint sprint, HttpServletRequest request)
+			throws SprintException {
 		int id = Integer.parseInt(request.getParameter("projectId"));
 		Project project = projectService.getProjectById(id);
 		int owner_id = (int) request.getSession().getAttribute("user_id");
 		sprint.setProject(project);
 		sprint.setOwner_id(owner_id);
 		sprint.setImgurl("/images/sprint.jpg");
-		ModelAndView modelAndView = new ModelAndView();
 		sprintService.saveSprint(sprint);
-		modelAndView.setViewName("redirect:/common/home#!/projectView/" + id);
-		return modelAndView;
+		return "redirect:/common/home#!/projectView/" + id;
 	}
 
-	@RequestMapping(value = "/common/sprintView", method = RequestMethod.GET)
-	public ModelAndView getIssueView() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("common/sprintView");
-		return modelAndView;
+	@GetMapping("/common/sprintView")
+	public String getIssueView() {
+		return "common/sprintView";
 	}
 
-	@RequestMapping(value = "/common/sprintView/{id}", method = RequestMethod.GET)
+	@GetMapping("/common/sprintView/{id}")
+	@ResponseBody
 	public Set<Issue> getIssueViewPost(@PathVariable(value = "id") int id) {
 		Sprint sprint = this.sprintService.findSprintById(id);
 		return sprint.getIssues();
